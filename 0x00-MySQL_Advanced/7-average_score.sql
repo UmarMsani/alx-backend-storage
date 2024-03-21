a85cb09c744ac76d4579b537039c5a037b29759f
UmarMsani <umuhammad220@gmail.com>
2024-03-21 16:34:17 +0000

    creates a stored procedure ComputeAverageScoreForUser

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,19 +1,14 @@
 -- Create stored procedure ComputeAverageScoreForUser
+-- Update user's average score
 DELIMITER $$
 
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    DECLARE avg_score DECIMAL(10, 2);
-
-    -- Compute average score
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Update user's average score
     UPDATE users
-    SET average_score = avg_score
+    SET average_score = (SELECT AVG(score)
+    FROM corrections
+    WHERE corrections.user_id = user_id)
     WHERE id = user_id;
 
 END $$
