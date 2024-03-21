cf185b6cf2da290ea42d47a25f23bff20400856e
UmarMsani <umuhammad220@gmail.com>
2024-03-21 15:02:17 +0000

    Add SQL script to create 'users' table with id, email, and name attributes

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- Create a table named 'users' if it does not exist
+-- If table exists, script will not fail, can be executed on any database
+
+CREATE TABLE IF NOT EXISTS users (
+	       id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	       email VARCHAR(255) NOT NULL UNIQUE,
+	       name VARCHAR(255)
+	);
